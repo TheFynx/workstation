3774241d40a14421b7145a52954d338efb7d2ba8
Levi Smith <levi@fynx.me>
2022-05-10 08:41:19 -0500

    adjust zsh history

--- manifests/files/exports.sh
+++ manifests/files/exports.sh
@@ -36,7 +36,6 @@ elif [[ "${SHELL}" =~ "zsh" ]]; then
   setopt incappendhistory
   setopt correct
   setopt caseglob
-  setopt histignorespace # skip cmds w/ leading space from history
 
   export HSTR_CONFIG=hicolor            # get more colors
   bindkey -s "\C-r" "\C-a hstr -- \C-j" # bind hstr to Ctrl-r (for Vi mode check doc)
